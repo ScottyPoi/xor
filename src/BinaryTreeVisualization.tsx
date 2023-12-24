c9b692c03ded5699ac9923d7a287eb1ccafbb2a7
ScottyPoi <scott.simpson@ethereum.org>
2023-12-24 15:26:10 -0700

    gpt update

--- src/BinaryTreeVisualization.tsx
+++ src/BinaryTreeVisualization.tsx
@@ -1,7 +1,7 @@
 // filename: BinaryTreeVisualization.tsx
-import React, { useState, useEffect, useRef } from 'react';
-import * as d3 from 'd3';
-import './BinaryTreeVisualization.css';
+import React, { useState, useEffect, useRef } from "react";
+import * as d3 from "d3";
+import "./BinaryTreeVisualization.css";
 
 interface ITreeNode {
   id: string;
@@ -11,9 +11,13 @@ interface ITreeNode {
   children?: ITreeNode[];
 }
 
-const generateTreeData = (depth: number, width: number, height: number): ITreeNode => {
-  const rootNodePosition = { x: width / 2, y: 100 };
-  const root: ITreeNode = { id: 'root', ...rootNodePosition, angle: 0 };
+const generateTreeData = (
+  depth: number,
+  width: number,
+  height: number
+): ITreeNode => {
+  const rootNodePosition = { x: width / 2, y: (5 * height) / 8 };
+  const root: ITreeNode = { id: "root", ...rootNodePosition, angle: 0 };
 
   const addChildren = (node: ITreeNode, level: number, angleRange: number) => {
     if (level < depth) {
@@ -22,9 +26,11 @@ const generateTreeData = (depth: number, width: number, height: number): ITreeNo
       const baseAngle = node.angle;
       node.children = [0, 1].map((i) => {
         const angle = baseAngle + ((i === 0 ? -1 : 1) * angleRange) / 2;
-        const x = rootNodePosition.x + distance * Math.sin((angle * Math.PI) / 180);
-        const y = rootNodePosition.y - distance * Math.cos((angle * Math.PI) / 180);
-        const childId = `${node.id}-${i === 0 ? 'L' : 'R'}`;
+        const x =
+          rootNodePosition.x + distance * Math.sin((angle * Math.PI) / 180);
+        const y =
+          rootNodePosition.y - distance * Math.cos((angle * Math.PI) / 180);
+        const childId = `${node.id}-${i === 0 ? "L" : "R"}`;
         const child: ITreeNode = { id: childId, x, y, angle };
         addChildren(child, level + 1, angleRange / 2);
         return child;
@@ -32,7 +38,7 @@ const generateTreeData = (depth: number, width: number, height: number): ITreeNo
     }
   };
 
-  const initialAngleRange = 45 + (120 ** ((depth - 1) ** (1 / 8) / (2 ** 0.5)));
+  const initialAngleRange = 45 + 120 ** ((depth - 1) ** (1 / 8) / 2 ** 0.5);
   addChildren(root, 1, initialAngleRange);
   return root;
 };
@@ -46,49 +52,59 @@ const BinaryTreeVisualization: React.FC = () => {
 
   useEffect(() => {
     const svg = d3.select(svgRef.current);
-    svg.selectAll('*').remove();
+    svg.selectAll("*").remove();
 
     const nodes = d3.hierarchy(treeData).descendants();
     const links = nodes.slice(1);
 
-    svg.selectAll('.link')
+    svg
+      .selectAll(".link")
       .data(links)
       .enter()
-      .append('line')
-      .attr('class', 'link')
-      .attr('x1', (d) => d.parent!.data.x)
-      .attr('y1', (d) => d.parent!.data.y)
-      .attr('x2', (d) => d.data.x)
-      .attr('y2', (d) => d.data.y)
-      .style('stroke', (d) => (d.data.id.endsWith('R') ? '#aaf' : '#faa'))
-      .style('stroke-width', 1);
+      .append("line")
+      .attr("class", "link")
+      .attr("x1", (d) => d.parent!.data.x)
+      .attr("y1", (d) => d.parent!.data.y)
+      .attr("x2", (d) => d.data.x)
+      .attr("y2", (d) => d.data.y)
+      .style("stroke", (d) => (d.data.id.endsWith("R") ? "#aaf" : "#faa"))
+      .style("stroke-width", 1);
 
-    const nodeEnter = svg.selectAll('.node')
+    const nodeEnter = svg
+      .selectAll(".node")
       .data(nodes)
       .enter()
-      .append('g')
-      .attr('class', 'node')
-      .attr('transform', (d) => `translate(${d.data.x},${d.data.y})`);
+      .append("g")
+      .attr("class", "node")
+      .attr("transform", (d) => `translate(${d.data.x},${d.data.y})`);
 
-    nodeEnter.append('circle')
-      .attr('r', 10)
-      .style('fill', (d) => (d.data.id.endsWith('R') ? '#00f' : '#f00'))
-      .style('stroke', '#fff')
-      .style('stroke-width', 1.5);
+    nodeEnter
+      .append("circle")
+      .attr("r", 10)
+      .style("fill", (d) => (d.data.id.endsWith("R") ? "#00f" : "#f00"))
+      .style("stroke", "#fff")
+      .style("stroke-width", 1.5);
 
-    nodeEnter.append('text')
-      .attr('dy', '.35em')
-      .style('text-anchor', 'middle')
-      .text((d) => (d.depth === depth ? d.data.id : ''));
-
+    nodeEnter
+      .append("text")
+      .attr("dy", ".35em")
+      .style("text-anchor", "middle")
+      .text((d) => (d.depth === depth ? d.data.id : ""));
   }, [depth, treeData]);
 
   return (
     <div className="binary-tree-container">
       <div className="controls">
-        <button className="button" onClick={() => setDepth(depth + 1)}>Increase Depth</button>
+        <button className="button" onClick={() => setDepth(depth + 1)}>
+          Increase Depth
+        </button>
         <span>Depth: {depth}</span>
-        <button className="button" onClick={() => setDepth(Math.max(depth - 1, 1))}>Decrease Depth</button>
+        <button
+          className="button"
+          onClick={() => setDepth(Math.max(depth - 1, 1))}
+        >
+          Decrease Depth
+        </button>
       </div>
       <svg ref={svgRef} width={width} height={height} className="tree-svg" />
     </div>
