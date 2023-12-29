6c6a9efa18cf7b0f678dadda5801490f45348253
ScottyPoi <scott.simpson@ethereum.org>
2023-12-29 16:09:55 -0700

    delete unused

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import VisualizationApp from "./circle";
 import ErrorBoundary from "./ErrorBoundary";
 
 import BinaryTreeVisualization from "./BinaryTreeVisualization";
